9c4ef9efae6a9b19923eda475ad58fc5365b3082
lasseschneider <lars.schneider@fh-erfurt.de>
2016-08-06 17:42:45 +0200

    insert comleted

--- Model/Session.java
+++ Model/Session.java
@@ -7,6 +7,7 @@ import java.sql.Timestamp;
  * Created by Lasse on 17.06.2016.
  */
 
+    //ToDo:Implementieren vom Ausloggen
 public class Session {
     private String Session_ID;
     private User user;
